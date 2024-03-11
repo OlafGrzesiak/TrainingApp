5b2a4e3bdad14271e7e1bb87703c51c9594e74bd
OlafGrzesiak <olaf.grzesiak@microsoft.wsei.edu.pl>
2024-03-11 18:16:11 +0100

    Interface changes

--- TrainingApp/MainForm.Designer.cs
+++ TrainingApp/MainForm.Designer.cs
@@ -1,39 +1,120 @@
-﻿namespace TrainingApp;
-
-partial class MainForm
+﻿namespace TrainingApp
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class MainForm
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
 
-        base.Dispose(disposing);
-    }
+        private void InitializeComponents()
+{
+    this.SuspendLayout();
 
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
+    // Ustawienia dla txtWorkoutType
+    txtWorkoutType = new System.Windows.Forms.TextBox
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
+        Location = new System.Drawing.Point(50, 30),
+        Size = new System.Drawing.Size(300, 30),
+        Font = new System.Drawing.Font("Segoe UI", 12F),
+        Text = "Enter workout type",
+        ForeColor = System.Drawing.Color.Gray,
+        BackColor = System.Drawing.Color.White,
+        BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    };
+    txtWorkoutType.Enter += TextBox_Enter;
+    txtWorkoutType.Leave += TextBox_Leave;
 
-    #endregion
+    // Ustawienia dla txtRepetitions
+    txtRepetitions = new System.Windows.Forms.TextBox
+    {
+        Location = new System.Drawing.Point(50, 70),
+        Size = new System.Drawing.Size(300, 30),
+        Font = new System.Drawing.Font("Segoe UI", 12F),
+        Text = "Enter repetitions",
+        ForeColor = System.Drawing.Color.Gray,
+        BackColor = System.Drawing.Color.White,
+        BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    };
+    txtRepetitions.Enter += TextBox_Enter;
+    txtRepetitions.Leave += TextBox_Leave;
+
+    // Ustawienia dla txtSets
+    txtSets = new System.Windows.Forms.TextBox
+    {
+        Location = new System.Drawing.Point(50, 110),
+        Size = new System.Drawing.Size(300, 30),
+        Font = new System.Drawing.Font("Segoe UI", 12F),
+        Text = "Enter sets",
+        ForeColor = System.Drawing.Color.Gray,
+        BackColor = System.Drawing.Color.White,
+        BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+    };
+    txtSets.Enter += TextBox_Enter;
+    txtSets.Leave += TextBox_Leave;
+
+    // Ustawienia dla btnAddWorkout
+    btnAddWorkout = new System.Windows.Forms.Button
+    {
+        Text = "Add Workout",
+        Location = new System.Drawing.Point(50, 150),
+        Size = new System.Drawing.Size(150, 40),
+        Font = new System.Drawing.Font("Segoe UI", 12F),
+        BackColor = System.Drawing.Color.Green,
+        ForeColor = System.Drawing.Color.White,
+        FlatStyle = System.Windows.Forms.FlatStyle.Flat
+    };
+    btnAddWorkout.Click += new System.EventHandler(this.btnAddWorkout_Click);
+
+    // Ustawienia dla btnDisplayWorkouts
+    btnDisplayWorkouts = new System.Windows.Forms.Button
+    {
+        Text = "Display Workouts",
+        Location = new System.Drawing.Point(200, 150),
+        Size = new System.Drawing.Size(150, 40),
+        Font = new System.Drawing.Font("Segoe UI", 12F),
+        BackColor = System.Drawing.Color.SteelBlue,
+        ForeColor = System.Drawing.Color.White,
+        FlatStyle = System.Windows.Forms.FlatStyle.Flat
+    };
+    btnDisplayWorkouts.Click += new System.EventHandler(this.btnDisplayWorkouts_Click);
+
+    this.Controls.AddRange(new System.Windows.Forms.Control[] { txtWorkoutType, txtRepetitions, txtSets, btnAddWorkout, btnDisplayWorkouts });
+
+    this.ResumeLayout(false);
+}
+
+private void TextBox_Enter(object sender, EventArgs e)
+{
+    var textBox = (TextBox)sender;
+    if (textBox.ForeColor == System.Drawing.Color.Gray)
+    {
+        textBox.Text = "";
+        textBox.ForeColor = System.Drawing.Color.Black;
+    }
+}
+
+private void TextBox_Leave(object sender, EventArgs e)
+{
+    var textBox = (TextBox)sender;
+    if (string.IsNullOrWhiteSpace(textBox.Text))
+    {
+        textBox.Text = textBox == txtWorkoutType ? "Enter workout type" : (textBox == txtRepetitions ? "Enter repetitions" : "Enter sets");
+        textBox.ForeColor = System.Drawing.Color.Gray;
+    }
+}
+
+
+        private System.Windows.Forms.TextBox txtWorkoutType;
+        private System.Windows.Forms.TextBox txtRepetitions;
+        private System.Windows.Forms.TextBox txtSets;
+        private System.Windows.Forms.Button btnAddWorkout;
+        private System.Windows.Forms.Button btnDisplayWorkouts;
+    }
 }
--- TrainingApp/MainForm.cs
+++ TrainingApp/MainForm.cs
@@ -3,88 +3,87 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
 
-namespace TrainingApp;
-
-public partial class MainForm : Form
+namespace TrainingApp
 {
-
-    private List<Workout> workouts = new List<Workout>();
-    
-    private TextBox txtWorkoutType;
-    private TextBox txtRepetitions;
-    private TextBox txtSets;
-    private Button btnAddWorkout;
-    private Button btnDisplayWorkouts;
-    public MainForm()
+    public partial class MainForm : Form
     {
-        InitializeComponent();
-        InitializeComponents();
-    }
+        private List<Workout> workouts = new List<Workout>();
 
-    private void InitializeComponents()
-    {
-        txtWorkoutType = new TextBox { Location = new System.Drawing.Point(150, 30), Size = new System.Drawing.Size(200, 20) };
-        txtRepetitions = new TextBox { Location = new System.Drawing.Point(150, 70), Size = new System.Drawing.Size(200, 20) };
-        txtSets = new TextBox { Location = new System.Drawing.Point(150, 110), Size = new System.Drawing.Size(200, 20) };
-
-        btnAddWorkout = new Button { Text = "Add Workout", Location = new System.Drawing.Point(50, 150) };
-        btnAddWorkout.Click += new EventHandler(btnAddWorkout_Click);
-
-        btnDisplayWorkouts = new Button { Text = "Display Workouts", Location = new System.Drawing.Point(200, 150) };
-        btnDisplayWorkouts.Click += new EventHandler(btnDisplayWorkouts_Click);
-
-        this.Controls.AddRange(new Control[] { txtWorkoutType, txtRepetitions, txtSets, btnAddWorkout, btnDisplayWorkouts });
-    }
-
-    private void MainForm_Load(object sender, EventArgs e)
-    {
-        this.Text = "Training Application";
-    }
-
-    private void btnAddWorkout_click(object sender, EventArgs e)
-    {
-        try
+        public MainForm()
         {
-            string wokroutType = txtWorkoutType.Text;
-            int repetitions = Convert.ToInt32(txtRepetitions.Text);
-            int sets = Convert.ToInt32(txtSets.Text);
-
-            workouts.Add(new Workout { Type = workoutType, Repetitions = repetitions, Sets = sets });
-
-            string message = $"Added workout: {workoutType}, {repetitions} repetitions, {sets} sets";
-
-            ClearInputFields();
-        }
-        catch (FormatException)
-        {
-            MessageBox.Show("Invalid input. Please enter numeric values for repetitions and sets.", "Error",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            InitializeComponent();
+            InitializeComponents();
         }
 
-        catch (Exception ex)
+        private void InitializeComponent()
         {
-            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
         }
 
-    }
-
-    private void btinDisplayWorkouts_Click(object sender, EventArgs e)
-    {
-        if (workouts.Count == 0)
+        private void InitializeFromComponents()
         {
-            MessageBox.Show("No workouts to display.", "Workout List", MessageBoxButtons.OK, MessageBoxIcon.Information);
+           
         }
-        else
+
+        private void MainForm_Load(object sender, EventArgs e)
         {
-            string workoutList = "Workout List:\n";
-            foreach (var workout in workouts)
+            this.Text = "Training Application";
+        }
+
+        private void btnAddWorkout_Click(object sender, EventArgs e)
+        {
+            try
             {
-                workoutList += $"{workout.Type}, {workout.Repetitions} repetitions, {workout.Sets} sets\n";
-            }
+                string workoutType = txtWorkoutType.Text;
+                int repetitions = Convert.ToInt32(txtRepetitions.Text);
+                int sets = Convert.ToInt32(txtSets.Text);
 
-            MessageBox.Show(workoutList, "Workout List", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                workouts.Add(new Workout { Type = workoutType, Repetitions = repetitions, Sets = sets });
+
+                
+                string message = $"Added workout: {workoutType}, {repetitions} repetitions, {sets} sets";           // For now, we'll just display the information in a MessageBox.
+                MessageBox.Show(message, "Workout Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ClearInputFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
+
+        private void btnDisplayWorkouts_Click(object sender, EventArgs e)
+        {
+            if (workouts.Count == 0)
+            {
+                MessageBox.Show("No workouts to display.", "Workout List", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string workoutList = "Workout List:\n";
+                foreach (var workout in workouts)
+                {
+                    workoutList += $"{workout.Type}, {workout.Repetitions} repetitions, {workout.Sets} sets\n";
+                }
+
+                MessageBox.Show(workoutList, "Workout List", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            txtWorkoutType.Text = "";
+            txtRepetitions.Text = "";
+            txtSets.Text = "";
+        }
+        
     }
 
-
+    public class Workout
+    {
+        public string Type { get; set; }
+        public int Repetitions { get; set; }
+        public int Sets { get; set; }
+    }
 }
